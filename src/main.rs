1cb1132b9f6c9bdaf48cba734cf8bb4b4a89573b
taijen <danielmadmonb@gmail.com>
2023-05-24 22:25:48 +0300

    working on cli

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,90 @@
-use argh::FromArgs;
+use clap::{Parser,Subcommand,Args};
+use tasker::taskerctl::{read_tasks_db,read_logs_db,rm_task,Task};
+
 fn main(){
-    let input:TopLevel = argh::from_env();
+    
+    let input = ArgsData::parse();
+    match input.input{
+        ArgsInput::Add(options) => {
+            //TODO:Unwarp safely and add task
+           let name = options.name;         
+        }
+        ArgsInput::Status => {
+            help();
+        }
+        ArgsInput::List => {
+            show_list();
+        }
+        ArgsInput::Remove(task_name) =>{
+            remove_task(task_name.name);
+        }
+    }
 }
-#[derive(FromArgs, PartialEq, Debug)]
-/// Top-level command.
-struct TopLevel {
-    #[argh(subcommand)]
-    add: Add,
+    
+#[derive(Debug,Parser,PartialEq)]
+#[clap(author,version,about)]
+#[command(author, version, about, long_about = None)]
+struct ArgsData{
+    #[clap(subcommand)]
+    input:ArgsInput
 }
 
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(subcommand)]
-enum Add {
-    Name(NameStruct),
-    Shell(ShellStruct),
+#[derive(Debug, Subcommand,PartialEq,Clone)]
+#[command(author, version, about, long_about = None)]
+enum ArgsInput{
+    ///show tasker-service status
+    Status,
+    ///show all the tasks
+    List,
+    ///Add new task
+    Add(AddOptions),
+    ///remove task by passing task's name
+    Remove(TaskName),
 }
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(subcommand, name = "Name")]
-#[argh(description = "add new task name")]
-struct NameStruct{
-    #[argh(option)]
-    ///name of the new task
+
+#[derive(Args,Debug,PartialEq,Clone)]
+struct  AddOptions{
+    ///add task name (required)
+    #[clap(short = 'n',long = "name")]
+    name:Option<String>,
+    ///add shell to run the command with (required)
+    #[clap(short = 's', long = "shell")]
+    shell:Option<String>,
+    ///add task command (required)
+    #[arg(short='c',long="command")]
+    command: Option<String>,
+    ///add comment
+    #[arg(long = "comment")]
+    comment: Option<String>,
+    ///add month of execution (optional)
+    #[arg(short='m',long="month")]
+    month: Option<i32>,
+    ///add day of month for execution (optional)
+    #[arg(short='d',long="day_month")]
+    day_of_month: Option<i32>,
+    ///add day of week for execution (optional)
+    #[arg(short='w',long="day_week")]
+    day_of_week: Option<i32>,
+    ///add hour of execution (optional)
+    #[arg(short='h',long="hour")]
+    hour: Option<i32>,
+    ///add minute of execution (optional)
+    #[arg(short='n',long="minute")]
+    minute: Option<i32>,
+}
+#[derive(Debug,PartialEq,Args,Clone)]
+struct TaskName{
     name:String
-}   
+}
 
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(description = "add shell name for new task")]
-#[argh(subcommand, name = "Shell")]
-struct ShellStruct{
-    #[argh(option)]
-    ///name of the shell to run the command on 
-    shell:String
-}   
+fn show_list(){
+    let task_db = read_tasks_db();
+
+}
+fn help(){
+
+}
+fn remove_task(task_name:String){
+    rm_task(task_name);
+    println!("task deleted");
+}
