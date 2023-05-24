2165b2353f76ff6b41db6b321b991eb6b6f75d13
taijen <danielmadmonb@gmail.com>
2023-05-24 21:55:49 +0300

    working on cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-use tasker::tasker_cli;
-fn main() {
-    
+use argh::FromArgs;
+fn main(){
+    let input:TopLevel = argh::from_env();
+}
+#[derive(FromArgs, PartialEq, Debug)]
+/// Top-level command.
+struct TopLevel {
+    #[argh(subcommand)]
+    add: Add,
 }
 
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum Add {
+    Name(NameStruct),
+    Shell(ShellStruct),
+}
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "Name")]
+#[argh(description = "add new task name")]
+struct NameStruct{
+    #[argh(option)]
+    ///name of the new task
+    name:String
+}   
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(description = "add shell name for new task")]
+#[argh(subcommand, name = "Shell")]
+struct ShellStruct{
+    #[argh(option)]
+    ///name of the shell to run the command on 
+    shell:String
+}   
